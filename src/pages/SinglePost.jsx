c75672e13d07545a8c38410399b76e7aa1561e8a
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-11-14 15:40:07 +0700

    Add SinglePost component with MainLayout, SecSinglePost, and SecBlogPost

---
+++ src/pages/SinglePost.jsx
@@ -0,0 +1,12 @@
+import SecBlogPost from "../components/section/SecBlogPost";
+import SecSinglePost from "../components/section/SecSinglePost";
+import MainLayout from "../layout/MainLayout";
+
+export default function SinglePost() {
+  return (
+    <MainLayout>
+      <SecSinglePost />
+      <SecBlogPost />
+    </MainLayout>
+  );
+}
