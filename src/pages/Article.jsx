cdfc409e2ac4135b97e21dcfc14e9949547438ef
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-11-14 15:39:54 +0700

    Add Article page component with MainLayout and SecArticle

---
+++ src/pages/Article.jsx
@@ -0,0 +1,10 @@
+import SecArticle from "../components/section/SecArticle";
+import MainLayout from "../layout/MainLayout";
+
+export default function Article() {
+  return (
+    <MainLayout>
+      <SecArticle />
+    </MainLayout>
+  );
+}
